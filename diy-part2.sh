561a50acb33cc6b49f81599b05c49e8a242118d8
WenJun <nczywq@qq.com>
2024-05-14 14:36:39 +0800

    sublime

--- diy-part2.sh
+++ diy-part2.sh
@@ -80,7 +80,7 @@ CONFIG_PACKAGE_luci-app-turboacc=y
 # 修改默认IP
 sed -i 's/192.168.1.1/10.0.2.1/g' package/base-files/files/bin/config_generate
 
-# 修改默认网关
+# 修改默认子网掩码
 sed -i 's/255.255.255.0/255.255.252.0/g' package/base-files/files/bin/config_generate
 
 # 修改默认主题
